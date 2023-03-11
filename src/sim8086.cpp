b8223dcddfca1689cb8e9a221d4c8354085f3e18
Radek Jurga <radoslaw.jurga@gmail.com>
2023-03-11 18:20:28 +0100

    Decode add, or, adc, sbb, and, sub, xor, cmp

--- src/sim8086.cpp
+++ src/sim8086.cpp
@@ -42,32 +42,47 @@ const char c_effectiveAddresses[8][8] =
     { "bx" },
 };
 
-bool tryReadFile(char* fileName, std::vector<uint8_t>* result)
+const char c_instructionNamesFor100000[8][4] =
+{
+    "add", // reg = 000
+    "or",  // reg = 001
+    "adc", // reg = 010
+    "sbb", // reg = 011
+    "and", // reg = 100
+    "sub", // reg = 101
+    "xor", // reg = 110
+    "cmp", // reg = 111
+};
+
+bool tryReadFile(char* pFileName, std::vector<uint8_t>* pResult)
 {
     std::ifstream file;
-    file.open(fileName, std::ifstream::binary | std::ifstream::in);
+    file.open(pFileName, std::ifstream::binary | std::ifstream::in);
     if (!file.good())
     {
-        std::cout << "Failed to open file " << fileName << ": " << std::strerror(errno) << "\n";
+        std::cout << "Failed to open file " << pFileName << ": " << std::strerror(errno) << "\n";
         return false;
     }
 
-    *result = std::vector<uint8_t>(std::istreambuf_iterator<char>(file), {});
+    *pResult = std::vector<uint8_t>(std::istreambuf_iterator<char>(file), {});
 
     return true;
 }
 
-size_t computeEffectiveAddress(
+// Fills the effectiveAddress string with the computed asm representation of the effective address.
+// Returns the size in bytes of the the displacement field.
+
+int computeEffectiveAddress(
     std::vector<uint8_t>::const_iterator byte,
     uint8_t mod,
     uint8_t rm,
-    std::string *effectiveAddress)
+    std::string* pEffectiveAddress)
 {
-    assert(effectiveAddress);
+    assert(pEffectiveAddress);
 
     // Returns the increment to the instruction pointer that accounts for the size of the displacement.
 
-    int increment = 0;
+    int displacementSize = 0;
 
     int16_t displacement = 0;
     const char* baseAndIndex = c_effectiveAddresses[rm];
@@ -85,7 +100,7 @@ size_t computeEffectiveAddress(
                 baseAndIndex = nullptr;
                 displacement = (byte[3] << 8) | byte[2];
 
-                increment += 2;
+                displacementSize = 2;
             }
         }
         break;
@@ -95,8 +110,7 @@ size_t computeEffectiveAddress(
             // Memory mode, 8-bit displacement follows
 
             displacement = static_cast<int8_t>(byte[2]);
-
-            increment += 1;
+            displacementSize = 1;
         }
         break;
 
@@ -105,35 +119,36 @@ size_t computeEffectiveAddress(
             // Memory mode, 16-bit displacement follows
 
             displacement = (byte[3] << 8) | byte[2];
-
-            increment += 2;
+            displacementSize = 2;
         }
         break;
 
-        default:
+        case 0b11:
         {
+            // Register mode, no effective address
+
             assert(false);
         }
         break;
     }
 
-    // Print effective address to string
+    // Write the effective address to string
 
-    *effectiveAddress += "[";
+    *pEffectiveAddress += "[";
 
     if (baseAndIndex)
     {
-        *effectiveAddress += baseAndIndex;
+        *pEffectiveAddress += baseAndIndex;
 
         if (displacement)
         {
             if (displacement > 0)
             {
-                *effectiveAddress += " + ";
+                *pEffectiveAddress += " + ";
             }
             else
             {
-                *effectiveAddress += " - ";
+                *pEffectiveAddress += " - ";
                 displacement = -displacement;
             }
         }
@@ -141,17 +156,131 @@ size_t computeEffectiveAddress(
 
     if (displacement)
     {
-        *effectiveAddress += std::to_string(displacement);
+        *pEffectiveAddress += std::to_string(displacement);
     }
 
-    *effectiveAddress += "]";
+    *pEffectiveAddress += "]";
 
-    return increment;
+    return displacementSize;
 }
 
-void decode(const std::vector<uint8_t>& input, std::string* output)
+// Fills the destination and source strings for instructions of the form "reg/memory and register to either".
+// Returns the increment that moves the instruction pointer to the next instruction.
+
+int decodeRegOrMemToRegOrMem(
+    std::vector<uint8_t>::const_iterator byte,
+    uint8_t d,
+    uint8_t w,
+    std::string* pDestination,
+    std::string* pSource)
 {
-    *output += "bits 16\n\n";
+    assert(pDestination);
+    assert(pSource);
+
+    int instructionPointerIncrement = 2;
+
+    uint8_t mod =  byte[1] >> 6;
+    uint8_t reg = (byte[1] >> 3) & 7;
+    uint8_t rm  =  byte[1]       & 7;
+
+    std::string& registerName = d ? *pDestination : *pSource;
+    std::string& otherName    = d ? *pSource      : *pDestination;
+
+    registerName = c_registerNames[w][reg];
+
+    if (mod == 0b11)
+    {
+        // Register mode (no displacement)
+
+        otherName = c_registerNames[w][rm];
+    }
+    else
+    {
+        instructionPointerIncrement += computeEffectiveAddress(byte, mod, rm, &otherName);
+    }
+
+    return instructionPointerIncrement;
+}
+
+// Appends the computed immediate value to the output string.
+// Returns the size in bytes of the data field containing the immediate value.
+
+int computeImmediate(std::vector<uint8_t>::const_iterator data, uint8_t s, uint8_t w, std::string* pImmediate)
+{
+    int dataSize = 1;
+
+    int16_t immediate = 0;
+
+    if (w)
+    {
+        // Value is word.
+
+        if (s)
+        {
+            // Sign extend 8-bit immediate data to 16-bits.
+
+            immediate = static_cast<int8_t>(data[0]);
+        }
+        else
+        {
+            // No sign extension.
+
+            immediate = (data[1] << 8) | data[0];
+            dataSize = 2;
+        }
+    }
+    else
+    {
+        // Value is 8-bits, no sign extension.
+
+        immediate = static_cast<uint16_t>(data[0]);
+    }
+
+    *pImmediate += std::to_string(immediate);
+
+    return dataSize;
+}
+
+// Fills the destination and source strings for instructions of the form "immediate to register/memory".
+// Returns the increment that moves the instruction pointer to the next instruction.
+
+int decodeImmToRegOrMem(
+    std::vector<uint8_t>::const_iterator byte,
+    uint8_t s,
+    uint8_t w,
+    std::string* pDestination,
+    std::string* pSource)
+{
+    assert(pDestination);
+    assert(pSource);
+
+    uint8_t mod = byte[1] >> 6;
+    uint8_t rm  = byte[1] & 7;
+
+    int displacementSize = 0;
+
+    if (mod == 0b11)
+    {
+        *pDestination = c_registerNames[w][rm];
+    }
+    else
+    {
+        *pSource = w ? "word " : "byte ";
+        displacementSize += computeEffectiveAddress(byte, mod, rm, pDestination);
+    }
+
+    const auto data = byte + 2 + displacementSize;
+    int dataSize = computeImmediate(data, s, w, pSource);
+
+    int instructionPointerIncrement = 2 + displacementSize + dataSize;
+    return instructionPointerIncrement;
+}
+
+// Decodes the machine code instructions in input and writes the disassembly in pOutput.
+
+void decodeInstructions(const std::vector<uint8_t>& input, std::string* pOutput)
+{
+    *pOutput += "bits 16\n\n";
 
     for (auto iInstruction = input.begin(), end = input.end(); iInstruction < end;)
     {
@@ -167,41 +296,67 @@ void decode(const std::vector<uint8_t>& input, std::string* output)
         uint8_t d      = (byte[0] >> 1) & 1;
         uint8_t w      =  byte[0]       & 1;
 
-        uint8_t mod = (iInstruction + 1 < end) ?  byte[1] >> 6      : 0;
-        uint8_t reg = (iInstruction + 1 < end) ? (byte[1] >> 3) & 7 : 0;
-        uint8_t rm  = (iInstruction + 1 < end) ?  byte[1]       & 7 : 0;
-
         // Disassemble instruction
 
         switch (opcode)
         {
+            // Instructions of the form "reg/memory and register to either"
+
+            case 0b000000: [[fallthrough]];
+            case 0b000010: [[fallthrough]];
+            case 0b000100: [[fallthrough]];
+            case 0b000110: [[fallthrough]];
+            case 0b001000: [[fallthrough]];
+            case 0b001010: [[fallthrough]];
+            case 0b001100: [[fallthrough]];
+            case 0b001110:
+            {
+                int iInstructionName = (opcode >> 1) & 7;
+                instructionName = c_instructionNamesFor100000[iInstructionName];
+
+                iInstruction += decodeRegOrMemToRegOrMem(byte, d, w, &destination, &source);
+            }
+            break;
+
+            // Instructions of the form "immediate to accumulator"
+
+            case 0b000001: [[fallthrough]];
+            case 0b000011: [[fallthrough]];
+            case 0b000101: [[fallthrough]];
+            case 0b000111: [[fallthrough]];
+            case 0b001001: [[fallthrough]];
+            case 0b001011: [[fallthrough]];
+            case 0b001101: [[fallthrough]];
+            case 0b001111:
+            {
+                int iInstructionName = (opcode >> 1) & 7;
+                instructionName = c_instructionNamesFor100000[iInstructionName];
+
+                destination = w ? "ax" : "al";
+
+                const auto data = byte + 1;
+                int dataSize = computeImmediate(data, d, w, &source);
+
+                iInstruction += 1 + dataSize;
+            }
+            break;
+
             // MOV register/memory to/from register
 
             case 0b100010:
             {
                 instructionName = "mov";
+                iInstruction += decodeRegOrMemToRegOrMem(byte, d, w, &destination, &source);
+            }
+            break;
 
-                if (mod == 0b11)
-                {
-                    // Register mode (no displacement)
+            // Instructions of the form "immediate to register/memory"
 
-                    const char* regName = c_registerNames[w][reg];
-                    const char* rmName  = c_registerNames[w][rm];
-
-                    destination = d ? regName : rmName;
-                    source      = d ? rmName  : regName;
-                }
-                else
-                {
-                    std::string& registerName     = d ? destination : source;
-                    std::string& effectiveAddress = d ? source      : destination;
-
-                    registerName = c_registerNames[w][reg];
-
-                    iInstruction += computeEffectiveAddress(byte, mod, rm, &effectiveAddress);
-                }
-
-                iInstruction += 2;
+            case 0b100000:
+            {
+                uint8_t reg = (byte[1] >> 3) & 7;
+                instructionName = c_instructionNamesFor100000[reg];
+                iInstruction += decodeImmToRegOrMem(byte, d, w, &destination, &source);
             }
             break;
 
@@ -214,7 +369,7 @@ void decode(const std::vector<uint8_t>& input, std::string* output)
                 std::string& effectiveAddress = d ? destination : source;
                 std::string& accumulator      = d ? source      : destination;
 
-                accumulator = "ax";
+                accumulator = w ? "ax" : "al";
 
                 uint16_t addr = (byte[2] << 8) | byte[1];
                 effectiveAddress = "[" + std::to_string(addr) + "]";
@@ -225,21 +380,22 @@ void decode(const std::vector<uint8_t>& input, std::string* output)
 
             // MOV immediate to register
 
-            case 0b101100:
-            case 0b101101:
-            case 0b101110:
+            case 0b101100: [[fallthrough]];
+            case 0b101101: [[fallthrough]];
+            case 0b101110: [[fallthrough]];
             case 0b101111:
             {
                 instructionName = "mov";
 
                 w = (byte[0] >> 3) & 1;
-                reg = byte[0] & 7;
+                uint8_t reg = byte[0] & 7;
+
                 destination = c_registerNames[w][reg];
 
-                int16_t data = w ? ((byte[2] << 8) | byte[1]) : static_cast<int8_t>(byte[1]);
-                source = std::to_string(data);
+                const auto data = byte + 1;
+                int dataSize = computeImmediate(data, 0, w, &source);
 
-                iInstruction += 2 + w;
+                iInstruction += 1 + dataSize;
             }
             break;
 
@@ -247,35 +403,14 @@ void decode(const std::vector<uint8_t>& input, std::string* output)
 
             case 0b110001:
             {
-                assert(d == 1);
-                assert(reg == 0);
-
                 instructionName = "mov";
-
-                size_t displacementOffset = 0;
-
-                if (mod == 0b11)
-                {
-                    destination += c_registerNames[w][rm];
-                }
-                else
-                {
-                    displacementOffset += computeEffectiveAddress(byte, mod, rm, &destination);
-                }
-
-                source = w ? "word " : "byte ";
-                int16_t data = w ?  ((byte[3 + displacementOffset] << 8) | byte[2 + displacementOffset]) :
-                                                       static_cast<int8_t>(byte[2 + displacementOffset]);
-                source += std::to_string(data);
-
-                iInstruction += 3 + displacementOffset + w;
+                iInstruction += decodeImmToRegOrMem(byte, d, w, &destination, &source);
             }
             break;
 
             default:
             {
-                std::cout << "Error decoding instruction: unknown opcode\n";
-                assert(false);
+                std::cerr << "Error decoding instruction: unknown opcode\n";
                 return;
             }
             break;
@@ -283,23 +418,23 @@ void decode(const std::vector<uint8_t>& input, std::string* output)
 
         // Print the asm instruction
 
-        *output += instructionName;
+        *pOutput += instructionName;
 
         if (!destination.empty())
         {
-            *output += " ";
-            *output += destination;
+            *pOutput += " ";
+            *pOutput += destination;
         }
 
         if (!source.empty())
         {
             assert(!destination.empty());
 
-            *output += ", ";
-            *output += source;
+            *pOutput += ", ";
+            *pOutput += source;
         }
 
-        *output += "\n";
+        *pOutput += "\n";
     }
 }
 
@@ -322,7 +457,7 @@ int main(int argc, char** argv)
     // Do the work
 
     std::string output;
-    decode(input, &output);
+    decodeInstructions(input, &output);
 
     // Write output
 
